ce78d77415fe2c6f08705392399ebf2dd3497a47
psi <cy.lonsido@gmail.com>
2026-09-14 22:50:22 +0800

    Supabase configuration and environment setup

--- src/pages/StoryList.jsx
+++ src/pages/StoryList.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react'
 import StoryCard from '../components/StoryCard'
+import { supabase } from '../services/supabase'
 
 const StoryList = () => {
+
+  useEffect(() => {
+    const testFetch = async () => {
+      const {data, error} = await supabase.from('stories').select('*')
+
+      if (error) console.error('Supabase error:', error.message)
+      else console.log('Fetched stories:', data[0])
+    }
+    testFetch()
+  },[])
+
   return (
     <div>
     </div>
